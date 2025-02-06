c5b96369f1a493812796d64b0b2edc3cbe15651b
홍석주 <hsj202201@gmail.com>
2025-02-06 16:58:53 +0900

    확인 버튼 짤림 해결

--- src/M-StoreRanding.js
+++ src/M-StoreRanding.js
@@ -9,6 +9,12 @@ function Mobile(props) {
       <div className="m-section">
         <img className="m-content" src="/img/M-siotRandingForStore.svg" alt="모바일 Store randing img" />
       </div>
+      <div>
+        <div className="footer">
+          <input type="email" placeholder="seeot@email.com / 이메일을 입력해주세요" onChange={(e) => { props.setEmail(e.target.value) }} />
+          <div className="m-okbutton" onClick={props.handleSubmit}>확인</div>
+        </div>
+      </div>
       <div className="footer">
         <input type="email" placeholder="seeot@email.com / 이메일을 입력해주세요" onChange={(e) => { props.setEmail(e.target.value) }} />
         <div className="m-okbutton" onClick={props.handleSubmit}>확인</div>
